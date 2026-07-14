510c30e83f051a44d941ef23df9536fbdd7d240a
Mauricio Zaffari <mauriciozaffari@gmail.com>
2026-07-14 11:50:08 -0300

    feat(cli): develoz new command with prompts + flags (T8)

--- lib/develoz/cli.rb
+++ lib/develoz/cli.rb
@@ -1,12 +1,84 @@
 # frozen_string_literal: true
 
 require "thor"
+require "tty-prompt"
+require_relative "version"
 
 module Develoz
   class CLI < Thor
+    OPT_IN_FLAGS = %i[api auth pwa push active_resource admin ui kamal docker db_backup].freeze
+    FLAG_LABELS = {
+      api: "API layer", auth: "authentication", pwa: "PWA support",
+      push: "push notifications", active_resource: "ActiveResource",
+      admin: "admin dashboard", ui: "develoz-ui", kamal: "Kamal deployment",
+      docker: "Docker setup", db_backup: "database backups"
+    }.freeze
+    RAILS_NEW_SKIPS = %w[--skip-test --skip-ci --skip-bundle].freeze
+
     desc "version", "Show version"
     def version
       puts "develoz #{Develoz::VERSION}"
+    end
+
+    desc "new APP_NAME", "Generate a new Develoz Rails app"
+    option :api, type: :boolean
+    option :auth, type: :boolean
+    option :pwa, type: :boolean
+    option :push, type: :boolean
+    option :active_resource, type: :boolean
+    option :admin, type: :boolean
+    option :ui, type: :boolean
+    option :kamal, type: :boolean
+    option :docker, type: :boolean
+    option :db_backup, type: :boolean
+    option :skip_pagy, type: :boolean
+    option :ruby, type: :string
+    option :rails, type: :string
+    option :yes, type: :boolean
+    def new(app_name)
+      resolved = resolve_flags
+      resolver = Develoz::VersionResolver.new
+      versions = resolver.resolve(ruby: options[:ruby], rails: options[:rails])
+      run_rails_new(app_name)
+      write_version_files(app_name, versions, resolver)
+      invoke_install(app_name, resolved)
+      puts "Created #{app_name} with Develoz Rails template."
+    end
+
+    private
+
+    def resolve_flags
+      OPT_IN_FLAGS.index_with { |flag| resolve_flag(flag) }
+    end
+
+    def resolve_flag(flag)
+      return options[flag] unless options[flag].nil?
+      return false if options[:yes]
+
+      prompt.yes?("Include #{FLAG_LABELS[flag]}?")
+    end
+
+    def prompt
+      @prompt ||= TTY::Prompt.new
+    end
+
+    def run_rails_new(app_name)
+      return if system("rails", "new", app_name, *RAILS_NEW_SKIPS)
+
+      raise "Failed to generate Rails app: #{app_name}"
+    end
+
+    def write_version_files(app_name, versions, resolver)
+      dir = File.expand_path(app_name)
+      resolver.write_tool_versions(dir, ruby: versions[:ruby])
+      resolver.write_ruby_version(dir, ruby: versions[:ruby])
+    end
+
+    def invoke_install(app_name, resolved)
+      opts = resolved.merge(skip_pagy: options[:skip_pagy])
+      Dir.chdir(app_name) do
+        Develoz::Generators::InstallGenerator.new([], opts, destination_root: File.expand_path(app_name)).install
+      end
     end
   end
 end
---
+++ spec/develoz/cli_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "develoz/cli"
+
+# rubocop:disable RSpec/AnyInstance, RSpec/MultipleExpectations
+RSpec.describe Develoz::CLI do
+  let(:resolver) { instance_double(Develoz::VersionResolver) }
+  let(:install_gen) { instance_double(Develoz::Generators::InstallGenerator) }
+
+  before do
+    allow(Develoz::VersionResolver).to receive(:new).and_return(resolver)
+    allow(resolver).to receive(:resolve).and_return({ ruby: "4.0.5", rails: "8.1" })
+    allow(resolver).to receive(:write_tool_versions)
+    allow(resolver).to receive(:write_ruby_version)
+    allow(Develoz::Generators::InstallGenerator).to receive(:new).and_return(install_gen)
+    allow(install_gen).to receive(:install)
+    allow_any_instance_of(described_class).to receive(:system).and_return(true)
+    allow(Dir).to receive(:chdir).and_yield
+  end
+
+  it "prints version" do
+    expect { described_class.start(%w[version]) }.to output(/develoz #{Develoz::VERSION}/o).to_stdout
+  end
+
+  it "generates app with --yes without prompting" do
+    expect { described_class.start(%w[new demo --yes]) }.to output(/Created demo/).to_stdout
+    expect(resolver).to have_received(:resolve).with(ruby: nil, rails: nil)
+    expect(resolver).to have_received(:write_tool_versions).with(anything, ruby: "4.0.5")
+    expect(install_gen).to have_received(:install)
+  end
+
+  it "passes --api flag through to install generator" do
+    described_class.start(%w[new demo --yes --api])
+    expect(Develoz::Generators::InstallGenerator).to have_received(:new)
+      .with([], hash_including(api: true), anything)
+  end
+
+  it "prompts without --yes" do
+    prompt_double = instance_double(TTY::Prompt)
+    allow(TTY::Prompt).to receive(:new).and_return(prompt_double)
+    allow(prompt_double).to receive(:yes?).and_return(true)
+    described_class.start(%w[new demo])
+    expect(prompt_double).to have_received(:yes?).at_least(:once)
+  end
+
+  it "raises on rails new failure" do
+    allow_any_instance_of(described_class).to receive(:system).and_return(false)
+    expect { described_class.start(%w[new demo --yes]) }.to raise_error(/Failed to generate/)
+  end
+end
+# rubocop:enable RSpec/AnyInstance, RSpec/MultipleExpectations
